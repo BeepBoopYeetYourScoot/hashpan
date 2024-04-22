b4b3d2f58843b2f0d8b562b4b17a7b73eda3364b
BeepBoopYeetYourScoot <zavorotnii.dr@gmail.com>
2024-04-22 14:28:32 +0700

    Environment settings

--- blacklistedCards/settings.py
+++ blacklistedCards/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xr=ianeu4tv*2)!trpa*+e0&-1)kr(_af#ec_w8kerudfk1mdu"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
